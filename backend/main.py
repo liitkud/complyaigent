1702ee8ab0b636b62ebd27a8393368e83d10a278
kuya-carlo <106532351+kuya-carlo@users.noreply.github.com>
2026-05-10 22:21:55 +0800

    fix: CORS issue on checker workflow

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,10 @@ app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,  # type: ignore
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=[
+        "http://localhost:3000",
+        "https://comply.kuyacarlo.dev",
+    ],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
