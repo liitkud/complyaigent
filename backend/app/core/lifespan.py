56bd7a3720ed2360b13f21e208218f1bdd0a68fa
Nana Mein <alfeoephraimquintay@gmail.com>
2026-07-04 13:24:15 +0800

    Feat: Open client connection with proper close connection socket for Groq api inference. Added app_state to hold those object.

--- backend/app/core/lifespan.py
+++ backend/app/core/lifespan.py
@@ -1,11 +1,31 @@
-from .db import init_db
+from typing import Optional
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.core.logging import logger
+from openai import AsyncOpenAI
+
+from .db import init_db
+from .config import settings
+class AppState:
+    groq_client: Optional[AsyncOpenAI] = None
+
+app_state = AppState()
+
+
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    init_db()
-    logger.info("Database initialized")
-    yield
+
+    try:
+        init_db()
+        logger.info("Database initialized")
+
+        app_state.groq_client = AsyncOpenAI(
+            base_url=settings.LLM_ENDPOINT, api_key=settings.LLM_API_KEY or "missing-key"
+        )
+        yield
+
+    finally:
+
+        await app_state.groq_client.close()
